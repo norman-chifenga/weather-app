c29b26512edab4b3cd724c680f379c42716dd7ab
norman <normanchifenga@gmail.com>
2022-10-27 15:19:38 +0200

    update README file

--- README.md
+++ README.md
@@ -1,70 +1,47 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+ # Weather app
 
-## Available Scripts
+ A React Weather app with OpenWeatherMap API
 
-In the project directory, you can run:
+## Features
 
-### `npm start`
+Find any city or place weather forecast
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Find user location weather
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+5 days forecast
 
-### `npm test`
+![image info](./src/assets/img/weather_app.gif)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+### INSTRUCTIONS TO RUN tHIS APP
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Clone this repository.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+```
+https://github.com/norman-chifenga/weather-app
+```
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Create a .env file at the root directory of the application and add the following variables to it. Register and obtain an API key with openweather.com.
+Assign the key to the REACT_APP_KEY
 
-### `npm run eject`
+```
+REACT_APP_KEY = ''
+REACT_APP_URL = 'http://api.openweathermap.org/data/2.5/forecast?'
+REACT_APP_ICON_URL = 'http://openweathermap.org/img/w/'
+```
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+Install dependencies
+```
+npm install
+```
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+To start the development mode run
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+```
+npm run dev
+```
+To build the application run
+```
+npm run build
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import { DailyData } from "./components/DailyData";
+import { DailyForecast } from "./components/DailyForecast";
+import { BsSearch } from "react-icons/bs";
+import { ImLocation } from "react-icons/im";
+const API_KEY = process.env.REACT_APP_KEY;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [weatherData, setWeatherData] = useState([]);
+    const locationInput = useRef("");
+    const [weatherIcons, setWeatherIcons] = useState([]);
+    const [message, setMessage] = useState("search to view data");
+    const [city, setCity] = useState(["", ""]);
+
+    //api call
+    const getDailyWeather = async (location_) => {
+        setMessage("loading...");
+        setWeatherData([]);
+        let how_to_search =
+            typeof location_ === "string"
+                ? `q=${location_}`
+                : `lat=${location_[0]}&lon=${location_[1]}`;
+        const api = `${
+            process.env.REACT_APP_URL + how_to_search
+        }&exclude=current,hourly,minutely,alerts&units=metric&appid=${API_KEY}`;
+        try {
+            await fetch(api)
+                .then((res) => res.json())
+                .then((result) => {
+                    if (result.cod === "404") {
+                        setMessage("City not found");
+                        setWeatherData([]);
+                    } else {
+                        const list = result.list.filter((item) =>
+                            item.dt_txt.includes("00:00:00")
+                        );
+                        setWeatherData(list);
+                        setWeatherIcons(
+                            list.map((item) => {
+                                return item.weather[0]["icon"] + ".png";
+                            })
+                        );
+                        setCity([result.city.country, result.city.name]);
+                    }
+                });
+        } catch (error) {
+            setMessage("search to view data");
+            setWeatherData([]);
+        }
+    };
+    //get my current location
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((location) => {
+                const { latitude, longitude } = location.coords;
+                getDailyWeather([latitude, longitude]);
+                locationInput.current.value = null;
+            });
+        } else {
+            setMessage("search to view data");
+            setWeatherData([]);
+        }
+    }
+
+    //handle search
+    const handleSearch = () => {
+        let location_ = locationInput.current.value;
+        if (location_ !== "") {
+            getDailyWeather(location_);
+        } else {
+            setWeatherData([]);
+            setMessage("Input field is empty");
+        }
+    };
+
+    return (
+        <div className="w-screen bg-cover bg-hero-img bg-fixed min-h-screen ">
+            <ul
+                style={{ background: "rgba(0,0,0,0.3)" }}
+                className="list-none w-full h-14 opacity-1 flex items-center justify-between md:px-10 px-7"
+            >
+                <li className="text-lg font-bold text-white">WeatherApp</li>
+                <li className="text-white">
+                    <ImLocation size="1.8rem" onClick={() => getLocation()} />
+                </li>
+            </ul>
+
+            <div className="flex w-[100%] justify-center   mx-auto margin mt-20">
+                <input
+                    type="text"
+                    ref={locationInput}
+                    placeholder="Search a city"
+                    className="rounded-tl-lg bg-gray-100 rounded-bl-lg focus:bg-white p-1 text-gray-500 min-w-[40%] outline-none h-10"
+                />
+                <BsSearch
+                    size="12"
+                    onClick={() => handleSearch()}
+                    className="text-gray-400 bg-gray-100 focus:bg-white  rounded-tr-lg rounded-br-lg p-1 hover:text-black w-9 h-10 outline-none hover:outline-none"
+                />
+            </div>
+
+            {weatherData.length !== 0 ? (
+                <DailyData
+                    weatherData={weatherData}
+                    city={city}
+                    dailyIcon={weatherIcons[0]}
+                />
+            ) : (
+                <div className="mt-[3em] text-center text-white w-[100%]">
+                    <p>{message}</p>
+                </div>
+            )}
+
+            <ul className="list-none flex justify-center pb-10 flex-wrap px-10 items-start">
+                {weatherData.map(
+                    (item, index) =>
+                        index !== 0 && (
+                            <DailyForecast
+                                key={index}
+                                weatherData={item}
+                                index={index}
+                                icon={weatherIcons}
+                            />
+                        )
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
